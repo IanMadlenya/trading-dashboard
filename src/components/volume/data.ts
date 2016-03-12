fee99b97d97e953545a5547399db4e7867d888df
Philip Yoo <philipyoo10@gmail.com>
2016-03-11 17:02:13 -0800

    Add interface for data

---
+++ src/components/volume/data.ts
@@ -0,0 +1,10 @@
+export interface Data {
+    adj_close: string,
+    close: string,
+    date: string,
+    high: string,
+    low: string,
+    open: string,
+    symbol: string,
+    volume: string
+}
