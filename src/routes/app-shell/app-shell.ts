c146c09b653a5f7471b58280bc647db8a8d269d5
Philip Yoo <philipyoo10@gmail.com>
2016-03-11 16:58:46 -0800

    Access yahoo finance api to grab data, parse data in app shell and display to test

--- src/routes/app-shell/app-shell.ts
+++ src/routes/app-shell/app-shell.ts
@@ -1,17 +1,41 @@
 import {Component, OnInit} from "angular2/core";
 import {CardComponent} from "../../components/card/card";
 import './app-shell.scss';
+import {VolumeComponent} from '../../components/volume/volume';
+import {StockInformationService} from '../../core/stock-information.service';
 
 @Component({
     selector: 'app-shell',
     template: require('./app-shell.html'),
-    directives: [CardComponent]
+    directives: [CardComponent, VolumeComponent],
+    providers: [StockInformationService]
 })
 export class AppShell implements OnInit {
+    response: Array<any>;
+
+    constructor(private _stockInformationService: StockInformationService) {}
     ngOnInit() {
+
+        this._stockInformationService.getData()
+            .subscribe(
+                data => {
+                    console.log(data.query.results.quote);
+                    this.response = data.query.results.quote;
+
+                    console.log(this.response[0]);
+
+                    // this.response = JSON.stringify(data.query.results.quote);
+                },
+                error => console.log(error)
+            )
     }
 
-    constructor() {
 
-    }
+
+
+
+
 }
+
+
+
