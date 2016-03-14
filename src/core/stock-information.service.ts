ed5ab3889b4b2f263ed6f3a749bbed530341412d
Philip Yoo <philipyoo10@gmail.com>
2016-03-14 16:44:30 -0700

    Remove postData method in stock information service.

--- src/core/stock-information.service.ts
+++ src/core/stock-information.service.ts
@@ -1,18 +1,10 @@
 import {Injectable} from 'angular2/core';
-import {Http, Headers} from 'angular2/http';
+import {Http} from 'angular2/http';
 import {Observable} from 'rxjs/Rx';
 
 @Injectable()
 export class StockInformationService {
     constructor (private _http: Http) {}
-
-    postData(data: any): Observable<any> {
-        const body = JSON.stringify(data);
-        const headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        return this._http.post('https://test-angular2.firebaseio.com/data.json', body, { headers: headers })
-            .map(response => response.json());
-    }
 
     getData(): Observable<any> {
         return this._http.get('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22AAPL%22%20and%20startDate%20%3D%20%222012-09-11%22%20and%20endDate%20%3D%20%222014-02-11%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=')
